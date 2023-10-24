bee4082955efa1934d495b384b5651761409d386
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-24 14:15:46 -0400

    Exercise 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
+console.log(document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render("Hello World");
