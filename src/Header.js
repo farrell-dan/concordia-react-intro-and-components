23c73a7d279be91cde6ef3ad240cd89dead0f844
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-24 16:24:49 -0400

    Exercise 5

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,17 @@
+const Colours = ["#335C67", "#FFF3B0", "#9E2A2B", "#540B0E", "white", "black"];
+
+const RandomIndex = Math.floor(Math.random() * Colours.length)
+const RandomColour = Colours[RandomIndex]
+
+const colorStyle = { "color" : RandomColour}
+
+console.log(colorStyle);
+
+
 const Header = () => {
   return (
     <header>
-      <h2>My First React Header.</h2>
+      <h2 style = { colorStyle }>My First React Header.</h2>
     </header>
   );
 };
